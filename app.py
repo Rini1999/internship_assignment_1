836c4dc53989672bbd52a6d60c569e4145a72bfa
Rini1999 <103992834+Rini1999@users.noreply.github.com>
2023-02-27 02:35:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config (layout="wide")
+
+st.sidebar.title("Pages")
+st.title("Students Performance Chart")
+st.header("Academic achievement as a performance outcome that indicates which students had accomplished specific goals.")
+st.subheader("This Students dataset consists of 33 features on the data of the students at the two schools. We want to know the relationship between each feature and its effect on students grades.")
+st.balloons()
